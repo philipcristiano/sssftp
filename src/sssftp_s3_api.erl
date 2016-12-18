20d31748ad74a0522234cc8fe766010ca4ba588b
Philip Cristiano <philipcristiano@Philips-MBP-2.home.philipcristiano.com>
2016-12-18 15:11:11 -0500

    Improve ls
    Filter out .
    Handle paths appropriately

--- src/sssftp_s3_api.erl
+++ src/sssftp_s3_api.erl
@@ -12,6 +12,8 @@
 -export([connectfun/3]).
 
 -record(state, {aws_bucket=undefined,
+                ls_info=undefined,
+                root=undefined,
                 user=undefined}).
 
 connectfun(User, _IP, _Method) ->
@@ -30,32 +32,38 @@ get_cwd(State) ->
     AWS_BUCKET = proplists:get_value(aws_bucket, State),
     io:format("CWD ~p~n", [{State, self()}]),
     {ok, User} = sssftp_user_session:get(self()),
+    Prefix = "uploads/" ++ User ++ "/",
     io:format("User is: ~p~n", [User]),
     {file:get_cwd(), #state{aws_bucket=AWS_BUCKET,
+                            root=Prefix,
                             user=User}}.
 
 is_dir(AbsPath, State) ->
     {filelib:is_dir(AbsPath), State}.
 
-list_dir(AbsPath, State) ->
+list_dir(AbsPath, State=#state{root=Prefix}) ->
     io:format("List dir ~p~n", [AbsPath]),
     AWS_BUCKET = State#state.aws_bucket,
-    User = State#state.user,
-    Prefix = "uploads/" ++ User,
     Options = [{prefix, Prefix}],
     Result = erlcloud_s3:list_objects(AWS_BUCKET, Options),
     Contents = proplists:get_value(contents, Result),
     Files = [proplists:get_value(key, X) || X <- Contents],
-    StrippedFiles = strip_prefix(Prefix, Files),
+    StrippedFiles = strip_path(Prefix, Files),
     io:format("Files ~p~n", [StrippedFiles]),
-    {{ok, StrippedFiles}, State}.
+    {{ok, StrippedFiles}, State#state{ls_info=Contents}}.
 
-strip_prefix(Prefix, Strings) when is_list(Prefix) ->
-    strip_prefix(string:len(Prefix) + 2, Strings);
-strip_prefix(Len, [H|T]) when is_integer(Len) ->
-    [string:sub_string(H, Len) | strip_prefix(Len, T)];
-strip_prefix(_Len, []) ->
+strip_path(Prefix, Strings) when is_list(Prefix) ->
+    strip_path(Prefix, string:len(Prefix) + 1, Strings).
+
+strip_path(Prefix, Len, [Prefix|T]) when is_integer(Len) ->
+    strip_path(Prefix, Len, T);
+strip_path(Prefix, Len, [H|T]) when is_integer(Len) ->
+    [strip_trailing_slash(string:sub_string(H, Len)) | strip_path(Prefix, Len, T)];
+strip_path(_Prefix, _Len, []) ->
     [].
+
+strip_trailing_slash(String) ->
+    string:strip(String, right, $/).
 
 make_dir(Dir, State) ->
     {file:make_dir(Dir), State}.
@@ -77,8 +85,32 @@ read_link(Path, State) ->
 
 read_link_info([], State) ->
     {{error, enoent}, State};
-read_link_info(_Path, State) ->
-    {{ok, #file_info{}}, State}.
+read_link_info(Path, State=#state{ls_info=Info, root=Prefix}) ->
+    FullPath = Prefix ++ string:sub_string(Path, 2),
+    io:format("Path: ~p~n", [FullPath]),
+    Data = find_content_from_key(FullPath, Info),
+    io:format("Info ~p~n", [Data]),
+    FileInfo = get_file_info_from_content(Data),
+    {{ok, FileInfo}, State}.
+
+get_file_info_from_content([]) ->
+    #file_info{type=directory};
+get_file_info_from_content([H|_]) ->
+    IsDir = lists:suffix("/", proplists:get_value(key, H)),
+    FileType = file_type(IsDir),
+    #file_info{size=proplists:get_value(size, H),
+               type=FileType}.
+
+file_type(true) ->
+    regular;
+file_type(false) ->
+    directory.
+
+find_content_from_key(Value, Contents) ->
+    lists:filter(fun(E) -> CValue = proplists:get_value(key, E),
+                           CValue =:= Value orelse (CValue =:= Value ++ "/")
+                           end,
+                 Contents).
 
 read_file_info(Path, State) ->
     {file:read_file_info(Path), State}.
