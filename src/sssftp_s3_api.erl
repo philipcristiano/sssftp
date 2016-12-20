bd50b1eb36912a865ac8b73513669aff33404f63
Philip Cristiano <philipcristiano@Philips-MBP-2.home.philipcristiano.com>
2016-12-19 20:24:01 -0500

    ls and cd working!

--- src/sssftp_s3_api.erl
+++ src/sssftp_s3_api.erl
@@ -12,21 +12,26 @@
 -export([connectfun/3]).
 
 -record(state, {aws_bucket=undefined,
+                dirs=undefined,
                 ls_info=undefined,
                 filenames=undefined,
-                root=undefined,
+                path=undefined,
+                s3_root=undefined,
                 user=undefined}).
 
 connectfun(User, _IP, _Method) ->
     io:format("User connected ~p~n", [{User, self()}]).
 
 close(IoDevice, State) ->
+    io:format("close~n"),
     {file:close(IoDevice), State}.
 
 delete(Path, State) ->
+    io:format("delete~n"),
     {file:delete(Path), State}.
 
 del_dir(Path, State) ->
+    io:format("del_dir~n"),
     {file:del_dir(Path), State}.
 
 get_cwd(State) ->
@@ -36,30 +41,94 @@ get_cwd(State) ->
     Root = "uploads/" ++ User,
     io:format("User is: ~p~n", [User]),
     {file:get_cwd(), #state{aws_bucket=AWS_BUCKET,
-                            root=Root,
+                            s3_root=Root,
                             user=User}}.
 
-is_dir(AbsPath, State=#state{root=Root}) ->
-    io:format("is_dir ~pn", [AbsPath]),
-    AWS_BUCKET = State#state.aws_bucket,
+is_dir(AbsPath, State0) ->
+    io:format("is_dir ~p~n", [AbsPath]),
+    Dir = filename:dirname(AbsPath),
+    {true, State1} = get_s3_path(Dir, State0),
 
     % Make sure our dirs end in /
     CleanAbsPath = strip_trailing_slash(AbsPath) ++ "/",
-    Prefix = Root ++ CleanAbsPath,
-    Options = [{prefix, Prefix}],
-    io:format("S3 Options ~p~n", [Options]),
-    Result = erlcloud_s3:list_objects(AWS_BUCKET, Options),
-    Contents = proplists:get_value(contents, Result),
-    Files = [proplists:get_value(key, X) || X <- Contents],
-    StrippedFiles = strip_path(Prefix, Files),
+    io:format("split dir ~p~n", [filename:split(CleanAbsPath)]),
     IsDir = lists:suffix("/", CleanAbsPath),
     io:format("IsDir ~p~n", [{CleanAbsPath, IsDir}]),
-    {true, State#state{ls_info=Contents, filenames=StrippedFiles}}.
+    {true, State1}.
 
-list_dir(AbsPath, State=#state{filenames=StrippedFiles}) ->
+get_s3_path(Path, State=#state{aws_bucket=Bucket, s3_root=S3Root}) ->
+    Prefix = S3Root ++ Path,
+    Options = [{prefix, Prefix}],
+    io:format("S3 Options ~p~n", [Options]),
+    Result = erlcloud_s3:list_objects(Bucket, Options),
+    Contents = proplists:get_value(contents, Result),
+    {true, State#state{ls_info=Contents,
+                       path=Path}}.
+
+filter_s3_abs_path("/", State) ->
+    io:format("filter s3 abs path 1 ~n"),
+    filter_s3_abs_path_("", State);
+filter_s3_abs_path([$/|Path], State) ->
+    io:format("filter s3 abs path 1.5 ~n"),
+    filter_s3_abs_path_(Path, State);
+filter_s3_abs_path(Path, State) ->
+    io:format("filter s3 abs path 2 ~p~n", [Path]),
+    filter_s3_abs_path_(Path, State).
+
+filter_s3_abs_path_(Path, #state{ls_info=Contents,
+                                         s3_root=S3Root}) ->
+    io:format("Filter for path_ ~p~n", [Path]),
+    AbsPath = filename:join([S3Root, Path]) ++ "/",
+    io:format("Filter for path ~p~n", [AbsPath]),
+    Files = [proplists:get_value(key, X) || X <- Contents],
+    APL = length(AbsPath),
+    LFiles = lists:filter(fun(El) -> length(El) >= APL end, Files),
+
+    StrippedObjs = strip_path(AbsPath, LFiles),
+    io:format("Stripped objs ~p~n", [StrippedObjs]),
+    FilteredFiles = filter_s3_files(StrippedObjs),
+    FilteredDirs = filter_s3_dirs(StrippedObjs),
+    {FilteredDirs, FilteredFiles}.
+
+
+% is_s3_dir(Path) ->
+%     SplitPath = filename:split(Path),
+%     lists:last(SplitPath) =:= "/".
+%
+
+filter_s3_files(Paths) ->
+    lists:filtermap(fun(El) ->
+                io:format("Filtering ~p~n", [El]),
+                case lists:last(El) of
+                    $/ -> false;
+                    _  -> filtered_for_multipart(El)
+                end
+              end, Paths).
+
+filtered_for_multipart(El) ->
+    Splits = filename:split(El),
+    case length(Splits) of
+        1 -> {true, El};
+        _ -> false
+    end.
+
+filter_s3_dirs(Paths) ->
+    A =lists:foldl(fun(El, Acc) ->
+                       Splits = filename:split(El),
+                       case length(Splits) of
+                           0 -> Acc;
+                           1 -> Acc;
+                           _ -> sets:add_element(hd(Splits), Acc)
+                       end
+                   end, sets:new(), Paths),
+    sets:to_list(A).
+
+list_dir(AbsPath, State) ->
     io:format("List dir ~p~n", [AbsPath]),
-    io:format("Files ~p~n", [StrippedFiles]),
-    {{ok, StrippedFiles}, State}.
+    {Dirs, Files} = filter_s3_abs_path(AbsPath, State),
+    LS = lists:append([Files, Dirs]),
+    io:format("Files ~p~n", [LS]),
+    {{ok, LS}, State}.
 
 strip_path(Prefix, Strings) when is_list(Prefix) ->
     strip_path(Prefix, string:len(Prefix) + 1, Strings).
@@ -67,7 +136,8 @@ strip_path(Prefix, Strings) when is_list(Prefix) ->
 strip_path(Prefix, Len, [Prefix|T]) when is_integer(Len) ->
     strip_path(Prefix, Len, T);
 strip_path(Prefix, Len, [H|T]) when is_integer(Len) ->
-    [strip_trailing_slash(string:sub_string(H, Len)) | strip_path(Prefix, Len, T)];
+    io:format("strip ~p ~p~n", [Prefix, H]),
+    [string:sub_string(H, Len) | strip_path(Prefix, Len, T)];
 strip_path(_Prefix, _Len, []) ->
     [].
 
@@ -121,7 +191,7 @@ find_content_from_key(Value, Contents) ->
 read_file_info(Path, State) ->
     read_info(Path, State).
 
-read_info(Path, State=#state{ls_info=Info, root=Prefix}) ->
+read_info(Path, State=#state{ls_info=Info, s3_root=Prefix}) ->
     FullPath = Prefix ++ string:sub_string(Path, 1),
     io:format("Path: ~p~n", [FullPath]),
     Data = find_content_from_key(FullPath, Info),
