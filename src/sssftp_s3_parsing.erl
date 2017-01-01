2a567f5675c8bde1b39463317ab38e5432def1f5
Philip Cristiano <philipcristiano@Philips-MBP-2.home.philipcristiano.com>
2017-01-01 17:54:58 -0500

    io:formats to log messages

--- src/sssftp_s3_parsing.erl
+++ src/sssftp_s3_parsing.erl
@@ -5,14 +5,16 @@
 -export([filter_s3_abs_path/3, is_dir/3]).
 
 filter_s3_abs_path(S3Root, "/", Contents) ->
+    lager:debug("Filter /"),
     filter_s3_abs_path(S3Root, "", Contents);
 
 filter_s3_abs_path(S3Root, [$/|T], Contents) ->
+    lager:debug("Filter T"),
     filter_s3_abs_path(S3Root, T, Contents);
 
 filter_s3_abs_path(S3Root, Path, Contents) ->
     AbsPath = filename:join([S3Root, Path]) ++ "/",
-    io:format("Filter for path ~p ~p~n", [Path, AbsPath]),
+    lager:debug("Filter for path ~p ~p", [Path, AbsPath]),
     Files = [proplists:get_value(key, X) || X <- Contents],
     APL = length(AbsPath),
     LFiles = lists:filter(fun(El) ->
@@ -21,15 +23,15 @@ filter_s3_abs_path(S3Root, Path, Contents) ->
                           end, Files),
 
     StrippedObjs = strip_path(AbsPath, LFiles),
-    io:format("Stripped objs ~p~n", [StrippedObjs]),
+    lager:debug("Stripped objs ~p", [StrippedObjs]),
     FilteredFiles = filter_s3_files(StrippedObjs),
     FilteredDirs = filter_s3_dirs(StrippedObjs),
     {FilteredDirs, FilteredFiles}.
 
 is_dir(S3Root, Path, Contents) ->
-    io:format("Parser is_dir ~p~n", [Path]),
+    lager:debug("Parser is_dir ~p", [Path]),
     AbsPath = normalize_dir_path(S3Root, Path),
-    io:format("Parser is_dir Abspath ~p~n", [AbsPath]),
+    lager:debug("Parser is_dir Abspath ~p", [AbsPath]),
     lists:any(fun(El) -> proplists:get_value(key, El) =:= AbsPath end, Contents).
 
 normalize_dir_path(S3Root, "/") ->
@@ -53,14 +55,14 @@ strip_path(Prefix, Strings) when is_list(Prefix) ->
 strip_path(Prefix, Len, [Prefix|T]) when is_integer(Len) ->
     strip_path(Prefix, Len, T);
 strip_path(Prefix, Len, [H|T]) when is_integer(Len) ->
-    io:format("strip ~p ~p~n", [Prefix, H]),
+    lager:debug("strip ~p ~p", [Prefix, H]),
     [string:sub_string(H, Len) | strip_path(Prefix, Len, T)];
 strip_path(_Prefix, _Len, []) ->
     [].
 
 filter_s3_files(Paths) ->
     lists:filtermap(fun(El) ->
-                io:format("Filtering ~p~n", [El]),
+                lager:debug("Filtering ~p", [El]),
                 case lists:last(El) of
                     $/ -> false;
                     _  -> filtered_for_multipart(El)
@@ -77,10 +79,11 @@ filtered_for_multipart(El) ->
 filter_s3_dirs(Paths) ->
     A =lists:foldl(fun(El, Acc) ->
                        Splits = filename:split(El),
+                       lager:debug("dir splits ~p", [Splits]),
                        case {length(Splits), lists:last(El)}  of
                            {0, _} -> Acc;
-                           {1, $/} -> io:format("1 match ~p~n", [Splits]),
-                                        sets:add_element(hd(Splits), Acc);
+                           {1, $/} -> lager:debug("1 match ~p", [Splits]),
+                                      sets:add_element(hd(Splits), Acc);
                            {1, _ } -> Acc;
                            _ -> sets:add_element(hd(Splits), Acc)
                        end
