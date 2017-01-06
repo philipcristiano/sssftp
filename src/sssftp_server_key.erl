667a45629c42f5e0c553c555c009c7bb6cec09b7
Philip Cristiano <philipcristiano@Philips-MBP-2.home.philipcristiano.com>
2017-01-06 06:35:00 -0500

    Improve logging when logging in
    Helpful to have a log of who logged in and if they were successful.
    
    Came up when s3 was unreachable from my laptop, this was hanging. Still
    needs a fix to ensure a timeout.

--- src/sssftp_server_key.erl
+++ src/sssftp_server_key.erl
@@ -1,6 +1,7 @@
 -module(sssftp_server_key).
 
 -behaviour(ssh_server_key_api).
+-compile([{parse_transform, lager_transform}]).
 
 
 -export([host_key/2,
@@ -13,15 +14,19 @@ host_key(Algorithm, DaemonOptions) ->
 is_auth_key(Key, User, _DaemonOptions) ->
     ok = sssftp_user_session:add(sssftp_user_session, User),
     OurKey = get_key_for_user(User),
-    is_valid_key(Key, OurKey).
+    IsValid = is_valid_key(Key, OurKey),
+    lager:info("Key for user '~p' valid? ~p", [User, IsValid]),
+    IsValid.
+
 
 get_key_for_user(User) ->
     KeyPath = "credentials/" ++ User ++ "/id_rsa.pub",
+    lager:debug("Getting key for user '~p' from'~p'", [User, KeyPath]),
     Bucket = os:getenv("AWS_BUCKET"),
     Object = erlcloud_s3:get_object(Bucket, KeyPath),
     Content = proplists:get_value(content, Object),
     [{Key, _Comment}|_Rest] = public_key:ssh_decode(Content, public_key),
-
+    lager:debug("Returning user key"),
     Key.
 
 is_valid_key(Key, Key) ->
