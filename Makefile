8de8ff41a55ab16cfb3293c0620dc154dede42e5
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-01-08 09:27:10 -0500

    First test for `get_cwd`

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ PROJECT = sssftp
 PROJECT_DESCRIPTION = New project
 PROJECT_VERSION = 0.1.0
 
+TEST_DEPS = meck
 SHELL_DEPS = sync
 LOCAL_DEPS = ssh
 DEPS = lager erlcloud
@@ -10,6 +11,7 @@ EUNIT_ERL_OPTS = -config sssftpd.config -s lager
 
 dep_lager = git https://github.com/basho/lager.git 3.2.4
 dep_erlcloud = git https://github.com/erlcloud/erlcloud.git 2.0.4
+dep_meck = git https://github.com/eproxus/meck.git 0.8.4
 
 SHELL_OPTS = -eval 'application:ensure_all_started(sssftp).' -config sssftpd
 
--- test/sssftp_s3_api_SUITE.erl
+++ test/sssftp_s3_api_SUITE.erl
@@ -17,10 +17,27 @@ groups() -> [{test_init,
 
 
 init_per_testcase(_, Config) ->
-    Config.
+    application:ensure_all_started(lager),
+
+    ok = meck:new(erlcloud_s3, []),
+    ok = meck:new(sssftp_user_session, []),
+
+    InitState = {initstate, [{aws_bucket, "TESTBUCKET"}]},
+    [InitState | Config].
 
 end_per_testcase(_, Config) ->
+    ok = meck:unload(erlcloud_s3),
+    ok = meck:unload(sssftp_user_session),
     Config.
 
-get_cwd_test(_Config) ->
+get_cwd_test(Config) ->
+    InitState = ?config(initstate, Config),
+    ok = meck:expect(erlcloud_s3, list_objects, fun(_, _) -> [] end),
+    ok = meck:expect(sssftp_user_session, get, fun(_) -> {ok, "USER"} end),
+
+    {{ok, "/"}, _State} = ?MUT:get_cwd(InitState),
+
+    true = meck:validate(erlcloud_s3),
+    true = meck:validate(sssftp_user_session),
+
     ok.
