65d4252bb25663f81446c97b2ece026e0380cfb8
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-01-21 20:34:22 -0500

    Test incorrect key returned

--- test/sssftp_server_key_SUITE.erl
+++ test/sssftp_server_key_SUITE.erl
@@ -5,7 +5,8 @@
 
 -export([all/0, groups/0, init_per_testcase/2, end_per_testcase/2]).
 
--export([is_auth_key_test/1]).
+-export([is_auth_key_test/1,
+         is_auth_key_incorrect_test/1]).
 
 -define(MUT, sssftp_server_key).
 
@@ -13,7 +14,8 @@ all() -> [{group, test_init}].
 
 groups() -> [{test_init,
              [],
-             [is_auth_key_test]}].
+             [is_auth_key_test,
+              is_auth_key_incorrect_test]}].
 
 
 init_per_testcase(_, Config) ->
@@ -37,3 +39,19 @@ is_auth_key_test(_Config) ->
     ok = meck:expect(public_key, ssh_decode, fun(_, public_key) -> [{key, undefined} | undefined] end),
 
     true = ?MUT:is_auth_key(key, "USER", DaemonOptions).
+
+is_auth_key_incorrect_test(_Config) ->
+    DaemonOptions = [{subsystems,
+                        [{"sftp",
+                            {ssh_sftpd,
+                                [{user_auth_server, user_auth_server}]}}]}],
+
+    ok = meck:new(erlcloud_s3, []),
+    ok = meck:new(sssftp_user_session, []),
+    ok = meck:new(public_key, []),
+
+    ok = meck:expect(erlcloud_s3, get_object, fun(_, _) -> [{contents, []}] end),
+    ok = meck:expect(sssftp_user_session, add, fun(user_auth_server, _) -> ok end),
+    ok = meck:expect(public_key, ssh_decode, fun(_, public_key) -> [{incorrect_key, undefined} | undefined] end),
+
+    false = ?MUT:is_auth_key(key, "USER", DaemonOptions).
