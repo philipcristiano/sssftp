29f432db3ebfa48b67eb0153428cecfbd5a7a8dd
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-02-02 05:54:05 -0500

    sssftp: Start API module
    Start module that will allow adding servers dynamically. Need to keep
    a list somewhere to register these in case the supervisor goes down :/

---
+++ src/sssftp.erl
@@ -0,0 +1,33 @@
+-module(sssftp).
+
+-export([add_sftp_server/1]).
+
+-compile([{parse_transform, lager_transform}]).
+
+add_sftp_server(Opts) ->
+    Port = proplists:get_value(port, Opts),
+    AWS_BUCKET = proplists:get_value(aws_bucket, Opts),
+    ok = lager:info("Starting on port ~p", [Port]),
+    UserAuthServerOpt = {user_auth_server, sssftp_user_session},
+    SFTPProc = {sftp_proc,
+                 {ssh, daemon, [Port, [{system_dir, "/tmp/"},
+                              {system_dir, "/etc/ssh"},
+                              {key_cb, sssftp_server_key},
+                              {connectfun, fun sssftp_s3_api:connectfun/3},
+                              {subsystems, [ssh_sftpd:subsystem_spec([
+                                    {file_handler, {sssftp_s3_api, [{aws_bucket, AWS_BUCKET}, UserAuthServerOpt]}},
+                                    UserAuthServerOpt,
+                                    {cwd, "/"}])
+                                        ]}]]},
+                permanent,
+                5000,
+                worker,
+                [sssftp_s3_api]},
+    UserSessionProc = {user_session, {sssftp_user_session, start_link, []},
+                       permanent,
+                       5000,
+                       worker,
+                       [user_session]},
+    {ok, _} = supervisor:start_child(sssftp_sup, UserSessionProc),
+    {ok, _} = supervisor:start_child(sssftp_sup, SFTPProc),
+    ok.
--- src/sssftp_app.erl
+++ src/sssftp_app.erl
@@ -7,32 +7,11 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	{ok, Pid} = sssftp_sup:start_link(),
+    {ok, Pid} = sssftp_sup:start_link(),
     {Port, _} = string:to_integer(os:getenv("PORT", "8989")),
-    AWS_BUCKET = os:getenv("AWS_BUCKET"),
-    ok = lager:info("Starting on port ~p", [Port]),
-    UserAuthServerOpt = {user_auth_server, sssftp_user_session},
-    SFTPProc = {sftp_proc,
-                 {ssh, daemon, [Port, [{system_dir, "/tmp/"},
-                              {system_dir, "/etc/ssh"},
-                              {key_cb, sssftp_server_key},
-                              {connectfun, fun sssftp_s3_api:connectfun/3},
-                              {subsystems, [ssh_sftpd:subsystem_spec([
-                                    {file_handler, {sssftp_s3_api, [{aws_bucket, AWS_BUCKET}, UserAuthServerOpt]}},
-                                    UserAuthServerOpt,
-                                    {cwd, "/"}])
-                                        ]}]]},
-                permanent,
-                5000,
-                worker,
-                [sssftp_s3_api]},
-    UserSessionProc = {user_session, {sssftp_user_session, start_link, []},
-                       permanent,
-                       5000,
-                       worker,
-                       [user_session]},
-    {ok, _} = supervisor:start_child(Pid, UserSessionProc),
-    {ok, _} = supervisor:start_child(Pid, SFTPProc),
+    Opts = [{aws_bucket, os:getenv("AWS_BUCKET")},
+            {port, Port}],
+    sssftp:add_sftp_server(Opts),
     {ok, Pid}.
 
 stop(_State) ->
